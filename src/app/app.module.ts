ed0d856b11c3c9618719ebb3401ee00edcc238a0
Rahul Singh <rahulrsingh09@gmail.com>
2017-10-06 23:39:56 +0530

    Styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { ServerSocket } from './service-socket.service';
 import { SocketUserComponent } from './socket-user.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import {TimeAgoPipe} from 'time-ago-pipe';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SocketUserComponent
+    SocketUserComponent,
+    TimeAgoPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule 
   ],
   providers: [ServerSocket],
   bootstrap: [AppComponent]
--- src/app/socket-user.component.ts
+++ src/app/socket-user.component.ts
@@ -2,11 +2,39 @@ import { element } from 'protractor';
 import { ServerSocket } from './service-socket.service';
 import { Component } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'socket-user',
   templateUrl: './SocketUser.component.html',
-  styleUrls: ['./SocketUser.component.css']
+  styleUrls: ['./SocketUser.component.css'],
+  animations: [
+    trigger('trending', [
+      state('DOWN', style({
+        'background-color': '#db3236'
+      })),
+      state('UP', style({
+        'background-color': '#3cba54'
+      })),
+      state('EQUAL', style({
+        'background-color': 'white'
+      })),
+      transition('DOWN => UP', [
+        style({transform: 'translateX(-100%)'}),
+        animate('1s ease-in')
+      ]),
+      transition('DOWN => UP', [
+        style({transform: 'translateY(-100%)'}),
+        animate('1s ease-in')
+      ])
+    ])
+  ],
 })
 export class SocketUserComponent {
   private socketSubscription: Subscription
@@ -29,7 +57,10 @@ ngOnInit() {
     if(this.prev){
       
       this.final = Object.keys(this.result).map((key) => {
-        return [key, this.result[key],this.prev[key] - this.result[key] < 0 ? 'UP' : 'DOWN'];
+ 
+        //return [key, this.result[key],this.prev[key] - this.result[key] < 0 ? 'UP' : 'DOWN',new Date()];
+        return [key, this.result[key], (this.prev[key] - this.result[key] == 0) ? 'EQUAL' : 
+                        (this.prev[key] - this.result[key] < 0) ? 'UP' : 'DOWN',new Date()];
       });
 
       for (let prevkey in this.prev){
